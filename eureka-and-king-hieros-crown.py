8a93566a825f666c0715ef0bef39bfcfb7e7a948
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-12-16 22:53:56 -0600

    progressing on Eureka challenge.

--- eureka-and-king-hieros-crown.py
+++ eureka-and-king-hieros-crown.py
@@ -1,1 +1,30 @@
 # https://www.101computing.net/eureka-and-king-hieros-crown/
+import re
+
+
+def calculate_density(mass, volume):
+    object_density = mass/volume
+    return object_density
+
+
+while True:
+    try:
+        mass = input('Please enter the mass of the metal in kilograms (exclude units)\n')
+        mass = float(re.sub(r'[a-z]+', '', mass, re.I))
+        if mass != '':
+            break
+        print('Invalid entry. Please enter a number only with no units\n')
+    except Exception as e:
+        print(e)
+while True:
+    try:
+        volume = input('Please enter the volume of the metal in m3 (exclude units)\n')
+        volume = float(re.sub(r'[a-z]+', '', mass, re.I))
+        if volume != '':
+            break
+        print('Invalid entry. Please enter a number only with no units\n')
+    except Exception as e:
+        print(e)
+
+density = calculate_density(mass, volume)
+print(density)
