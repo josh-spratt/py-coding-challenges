1515b834cca0db363343f3eda3e55f58f2b8a079
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-12-19 23:49:22 -0600

    Typing text effects (#6)
    * not really a challenge, just learning
    
    * completed the integersums challenge (this one was easy)
    
    * first poc for caesar cipher

---
+++ caesar-cipher.py
@@ -0,0 +1,41 @@
+""" Caesar Cipher
+    A Caesar cipher is a simple substitution cipher in which each letter of the
+    plain text is substituted with a letter found by moving n places down the
+    alphabet. For example, assume the input plain text is the following:
+
+        abcd xyz
+
+    If the shift value, n, is 4, then the encrypted text would be the following:
+
+        efgh bcd
+
+    You are to write a function that accepts two arguments, a plain-text
+    message and a number of letters to shift in the cipher. The function will
+    return an encrypted string with all letters transformed and all
+    punctuation and whitespace remaining unchanged.
+
+    Note: You can assume the plain text is all lowercase ASCII except for
+    whitespace and punctuation.
+"""
+n = 5
+alphabet_string = 'abcdefghijklmnopqrstuvwxyz'
+alphabet_list = []
+for letter in alphabet_string:
+    alphabet_list.append(letter)
+
+input_string = 'this is an example of the input string'
+output_string = ""
+
+for letter in input_string:
+    if letter != " ":
+        if letter in alphabet_list:
+            list_index = alphabet_list.index(letter)
+            if list_index <= len(alphabet_list)-n-1:
+                letter = alphabet_list[list_index+n]
+                output_string += letter
+            if list_index >= len(alphabet_list)-n:
+                letter = alphabet_list[(list_index+n)-26]
+                output_string += letter
+    if letter == " ":
+        output_string += letter
+print(output_string)
